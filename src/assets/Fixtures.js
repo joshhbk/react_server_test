31c9b22be2eb3366f0b62d093e5d6b4eb0e9145f
Joshua Hughes <joshuah@tcd.ie>
2017-11-17 17:25:59 +0000

    move presentational component out of App.js

---
+++ src/assets/Fixtures.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+class Fixtures extends Component {
+
+  constructor(props) {
+    super(props);
+
+    let fixtures;
+
+    if (props.staticContext) {
+      fixtures = props.staticContext.data;
+    } else {
+      fixtures = window.__data__;
+      delete window.__data__;
+    }
+
+    this.state = { fixtures }
+  }
+
+  testClick() {
+    console.log('test')
+  }
+
+  fetchInfo() {
+    console.log('ran')
+  }
+
+  componentDidMount() {
+    const intervalFetch = setInterval(this.fetchInfo, 30000);
+    this.setState({intervalFetch});
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalFetch);
+  }
+
+  render() {
+    return (
+      <div className={css(styles.grey, styles.hover)}>
+        <div>
+          <h1>Test</h1>
+        </div>
+        <ul>
+          {this.state.fixtures.map((fixture, i) => {
+            return <li key={i}><a onClick={this.testClick}>{fixture.competitionDate}</a></li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  grey: {
+    backgroundColor: 'grey'
+  },
+  hover: {
+    ':hover': {
+      backgroundColor: 'red'
+    }
+  }
+});
+export default Fixtures;
